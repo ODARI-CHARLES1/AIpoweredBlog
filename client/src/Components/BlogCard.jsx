4f8f0cf4ee94e78b8070ee99d9b7856a6db72478
ODARI-CHARLES1 <daymondodari68@gmail.com>
2025-06-08 21:34:36 +0300

    imlementation of blog page

--- client/src/Components/BlogCard.jsx
+++ client/src/Components/BlogCard.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { useNavigate } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const BlogCard = ({blog}) => {
      const {title,description,category,image,_id}=blog;
      const navigate=useNavigate()
+     useEffect(() => {
+    AOS.init({
+      duration: 1000, 
+      once:false, 
+      mirror:true,
+    });
+    AOS.refresh();
+  }, []);
   return (
-        <div onClick={()=>navigate(`blog/${_id}`)} className='w-full roundeng-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300'>
+        <div data-aos="fade-left" onClick={()=>navigate(`blog/${_id}`)} className='w-full roundeng-lg overflow-hidden shadow hover:scale-102 hover:shadow-primary/25 duration-300'>
             <div>
                 <img src={image} alt="image" />
                 <span className='ml-5 mt-4 px-3 py-1 inline-block bg-primary/20 rounded-full text-primary text-xs'>{category}</span>
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { assets } from '../assets/assets'
-
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 const Header = () => {
+    useEffect(()=>{
+        AOS.init({
+            duration:1000,
+            once:false,
+            mirror:true
+        })
+        AOS.refresh();
+    },[])
   return (
-    <div className='mx-8 sm-mx-16 xl-mx-24 relative'>
+    <div  className='mx-8 sm-mx-16 xl-mx-24 relative'>
         <div className='text-center mt-20 mb-8'>
             <div className='inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary'>
-                <p>New: AI feature integrated</p>
+                <p data-ao="fade-up">New: AI feature integrated</p>
                 <img src={assets.star_icon} className='w-2.5' alt="star-icon" />
             </div>
             <h1 className='text-3xl sm:text-6xl font-semibold sm:leading-16 text-gray-700 '>
